0684f496a167fab6644e55ab45dbb9f841029bb9
Andre Puschmann <andre.puschmann@tu-ilmenau.de>
2014-01-30 12:24:40 +0100

    implement a configurable logging file

--- iris/Iris.h
+++ iris/Iris.h
@@ -16,12 +16,12 @@
  * it under the terms of the GNU Lesser General Public License as
  * published by the Free Software Foundation, either version 3 of
  * the License, or (at your option) any later version.
- * 
+ *
  * Iris is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- * 
+ *
  * A copy of the GNU Lesser General Public License can be found in
  * the LICENSE file in the top-level directory of this distribution
  * and at http://www.gnu.org/licenses/.
@@ -62,6 +62,9 @@ IRIS_DLL bool IRISSetContRepository(std::string rep);
 
 /// Set the log level
 IRIS_DLL bool IRISSetLogLevel(std::string level);
+
+/// Set the logging file
+IRIS_DLL bool IRISSetLoggingFile(std::string file);
 
 /** Load the radio
 *
--- src/Iris.cpp
+++ src/Iris.cpp
@@ -16,12 +16,12 @@
  * it under the terms of the GNU Lesser General Public License as
  * published by the Free Software Foundation, either version 3 of
  * the License, or (at your option) any later version.
- * 
+ *
  * Iris is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- * 
+ *
  * A copy of the GNU Lesser General Public License can be found in
  * the LICENSE file in the top-level directory of this distribution
  * and at http://www.gnu.org/licenses/.
@@ -43,7 +43,7 @@ using namespace iris;
 boost::scoped_ptr< System > theSystem;
 
 string getName()
-{   
+{
     return "Iris";
 }
 
@@ -115,6 +115,18 @@ bool IRISSetLogLevel(std::string level)
         return false;
     }else{
         theSystem->setLogLevel(level);
+        return true;
+    }
+}
+
+bool IRISSetLoggingFile(std::string file)
+{
+    if(theSystem == NULL)
+    {
+        LOG(LERROR) << "System has not been initialized.";
+        return false;
+    }else{
+        theSystem->setLoggingFile(file);
         return true;
     }
 }
